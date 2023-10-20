c15bb00be8b042c866f0dfb91ca4fb3fa00c4c61
Mario Vago Marzal <mariovagomarzal@gmail.com>
2023-10-20 19:21:49 +0200

    Añade las funciones para calcular el polinomio de Lagrange

---
+++ lagrange/lagrange.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+
+def diferencias_divididas(nodos, f_nodos):
+    """
+    Calcula las diferencias divididas de Newton
+    :param nodos: nodos
+    :param f_nodos: f(nodos)
+    :return: tabla de diferencias divididas
+    """
+    n = len(nodos)
+    tabla = np.zeros((n, n))
+    tabla[:, 0] = f_nodos
+
+    for j in range(1, n):
+        for i in range(n - j):
+            tabla[i][j] = (tabla[i + 1][j - 1] - tabla[i][j - 1])\
+                / (nodos[i + j] - nodos[i])
+
+    # Devuelve la diagonal principal
+    return tabla[0, :]
+
+def lagrange(x, nodos, f_nodos):
+    """
+    Evalua el polinomio interpolador de Lagrange
+    :param x: punto a evaluar
+    :param nodos: nodos
+    :param f_nodos: f(nodos)
+    :return: p(x)
+    """
+    n = len(nodos)
+    p = 0
+    for i in range(n):
+        l = 1
+        for j in range(n):
+            if i != j:
+                l *= (x - nodos[j]) / (nodos[i] - nodos[j])
+        p += f_nodos[i] * l
+    return p
